27069c60b548f67a31889e4e5476d2233fc892ce
tonycapone <anthonyrhowell@gmail.com>
2017-03-30 09:24:13 -0500

    cleanup and tweaks

--- handler.js
+++ handler.js
@@ -1,27 +1,18 @@
 "use strict";
 
 var forwarder = require('aws-lambda-ses-forwarder')
-var defaultConfig = {
-  fromEmail: "noreply@example.com",
-  subjectPrefix: "",
-  emailBucket: "s3-bucket-name",
-  emailKeyPrefix: "emailsPrefix/",
-  forwardMapping: {
-    "info@example.com": [
-      "example.john@example.com",
-      "example.jen@example.com"
-    ],
-    "abuse@example.com": [
-      "example.jim@example.com"
-    ],
-    "@example.com": [
-      "example.john@example.com"
-    ],
-    "info": [
-      "info@example.com"
-    ]
-  }
-};
+
 module.exports.handle = (event, context, callback) => {
-  forwarder.handler(event, context, callback, defaultConfig)
+  var overrides = {
+    config: {
+      emailKeyPrefix: "",
+      emailBucket: process.env.emailBucket,
+      forwardMapping: {
+        "@ezmails.xyz": [
+          "anthonyrhowell@gmail.com"
+        ],
+      }
+    }
+  };
+  forwarder.handler(event, context, callback, overrides)
 };
