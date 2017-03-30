3f8531961717a6c34ad06464cc4bb4e033a0ef33
tonycapone <anthonyrhowell@gmail.com>
2017-03-30 11:21:38 -0500

    handler wasn't passing through bucket name

--- handler.js
+++ handler.js
@@ -1,9 +1,10 @@
 "use strict";
 var fs = require('fs')
 var forwarder = require('aws-lambda-ses-forwarder')
+var yaml = require('js-yaml');
 
 module.exports.handle = (event, context, callback) => {
   var overrides = yaml.safeLoad(fs.readFileSync('config.yml', 'utf8'));
-  overrides.emailBucket = process.env.emailBucket
+  overrides.config.emailBucket = process.env.emailBucket
   forwarder.handler(event, context, callback, overrides)
 };
