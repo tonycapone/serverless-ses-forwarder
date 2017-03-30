50da903fe79728962674e594d4d9652824e4cfcd
tonycapone <anthonyrhowell@gmail.com>
2017-03-30 11:07:41 -0500

    updates for yaml config

--- handler.js
+++ handler.js
@@ -4,6 +4,6 @@ var forwarder = require('aws-lambda-ses-forwarder')
 
 module.exports.handle = (event, context, callback) => {
   var overrides = yaml.safeLoad(fs.readFileSync('config.yml', 'utf8'));
-    
+  overrides.emailBucket = process.env.emailBucket
   forwarder.handler(event, context, callback, overrides)
 };
