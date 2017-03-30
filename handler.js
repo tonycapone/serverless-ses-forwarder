83a902915a2d0a6863a39aab0178b3fd7a26d9ca
tonycapone <anthonyrhowell@gmail.com>
2017-03-30 11:04:45 -0500

    updated to use yml file for config

--- handler.js
+++ handler.js
@@ -1,30 +1,9 @@
 "use strict";
-
+var fs = require('fs')
 var forwarder = require('aws-lambda-ses-forwarder')
 
 module.exports.handle = (event, context, callback) => {
-  var overrides = {
-    config: {
-      fromEmail: "noreply@example.com",
-      subjectPrefix: "",
-      emailBucket: "s3-bucket-name",
-      emailKeyPrefix: "emailsPrefix/",
-      forwardMapping: {
-        "info@example.com": [
-          "example.john@example.com",
-          "example.jen@example.com"
-        ],
-        "abuse@example.com": [
-          "example.jim@example.com"
-        ],
-        "@example.com": [
-          "example.john@example.com"
-        ],
-        "info": [
-          "info@example.com"
-        ]
-      }
-    }
-  };
+  var overrides = yaml.safeLoad(fs.readFileSync('config.yml', 'utf8'));
+    
   forwarder.handler(event, context, callback, overrides)
 };
